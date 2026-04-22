63181a398f51e3408f8dd086da11b0cf95f06221
Harenderchhoker31 <harenderchhoker31@gmail.com>
2026-04-22 14:18:40 +0530

    user schema created

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ require("dotenv").config()
 const app =  require('./src/app.js')
 const connet_to_db = require('./src/config/database.js')
 
-connet_to_db()
+connet_to_db()  
 app.listen(3000, () => console.log('Server running on port 3000'))
---
+++ src/models/user.model.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose")
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        unique:[true,"username already taken"],
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:[true,"email already registered"]
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const userModel = mongoose.model("user",userSchema)
+
+module.exports=userModel

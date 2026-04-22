25414ba94b364f1ae9fb6e964d92bf61194a7655
Harenderchhoker31 <harenderchhoker31@gmail.com>
2026-04-22 22:21:48 +0530

    login page copmpleted

--- Frontend/src/features/auth/pages/login.jsx
+++ Frontend/src/features/auth/pages/login.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
+import '../form.scss'
 
 const login = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
   return (
     <main>
         <div className='formcontainer'>
             <h1>Login</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='input-group'>
-                    <label for="email">email</label>
+                    <label for="email">Email</label>
                     <input type="email" id="email" name="email" idplaceholder='Enter Your Email'/>
                 </div>
                 <div className='input-group'>
-                    <label for="password">Password</label>"
+                    <label for="password">Password</label>
                     <input type="password" id="password" name="password" idplaceholder='Enter Your Password'/>
                 </div>
-                <button className=''>Login</button>
+                <button className='button primary-button'>Login</button>
             </form>
         </div>
     </main>

3d6cf7b8cfe91bb8dd3e1a3f96f97efe0e6e1c25
Harenderchhoker31 <harenderchhoker31@gmail.com>
2026-04-22 21:55:07 +0530

    login ,register template

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,10 @@
-import 
+import {RouterProvider} from "react-router-dom"
+import {router} from "./app.routes.jsx"
 
 function App() {
 
   return (
-    <>
-     
-    </>
+   <RouterProvider router={router}/>
   )
 }
 
--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,5 +1,19 @@
-import {createBrowserRouter} from "react-router";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 import Login from "./features/auth/pages/login";
 import Register from "./features/auth/pages/register";
 
-export const router=createBrowserRouter([])
+export const router=createBrowserRouter([
+     {
+        path:'/',
+        element:<Navigate to='/login'/>
+     },
+     {
+        path:'/login',
+        element:<Login/>
+     },
+     {
+        path:'/register',
+        element:<Register/>
+
+     }
+])
